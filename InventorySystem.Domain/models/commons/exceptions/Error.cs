1e1e20d8092d8d62f3bc6fd5285482c55f5b842e
Juan Pablo Cano <canojuan0415@gmail.com>
2023-01-06 18:53:08 -0500

    Feature: Error message class created

---
+++ InventorySystem.Domain/models/commons/exceptions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace InventorySystem.Domain.models.commons.exceptions;
+
+public static class Error
+{
+    public static readonly string ProductNotFound =  "Product not found";
+    public static readonly string EntityIsRequired =  "Entity is required";
+    
+}
